0d43c1235946732613e0db96c9f837759d7aad6d
tristan <mousy93@hotmail.com>
2021-04-22 17:44:27 +0800

    added id to 9am slot in collum
    add id for each time

--- assests/planner.js
+++ assests/planner.js
@@ -1,20 +1,27 @@
 /*
 WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-
-IF type text in textArea
-WHEN save button is pressed
-THEN save the text to local storage
-IF page is refreshed 
+THEN the current day is displayed at the top of the calendar    
+ 
+IF type text in textArea      --TODO
+WHEN save button is pressed            
+THEN save the text to local storage    
+IF page is refreshed                     --TODO
 THEN text in local storage stays in textarea
 
-WHEN I view the timeblocks for that day
+WHEN I view the timeblocks for that day           --TODO
 THEN each timeblock is color coded to indicate whether it
 is in the past, present, or future
    */
 
-  var timeDate = moment().format('MMMM Do YYYY, h:mm:ss a');
+  const timeDate = moment().format('MMMM Do YYYY, h:mm a');
     $("#currentDay").text(timeDate);
     console.log(timeDate)
 
+  let save = document.getElementsByClassName("saveBtn")
 
+  $(save).click(function (e) { 
+    e.preventDefault();
+    console.log("click click")
+
+  });
+    
